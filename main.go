29f0e796c07294d000c04bc358cdf8b40e79cc32
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-02-19 08:42:25 +1100

    Define `CSPReport` struct
    Outlines the structure of our `CSPReport` and which field are expected.

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+type CSPReport struct {
+	Body struct {
+		DocumentURI        string `json:"document-uri"`
+		Referrer           string `json:"referrer"`
+		BlockedURI         string `json:"blocked-uri"`
+		ViolatedDirective  string `json:"violated-directive"`
+		EffectiveDirective string `json:"effective-directive"`
+		OriginalPolicy     string `json:"original-policy"`
+	} `json:"csp-report"`
+}
+
 func main() {
 	http.HandleFunc("/", handleViolationReport)
 	http.ListenAndServe(":80", nil)
